3b9314a10200b18029db6c43186774e1fe5c8329
diddnwjd <117058374+diddnwjd@users.noreply.github.com>
2023-02-09 21:16:25 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/paintourcolor/odle/OdleApplication.java
@@ -0,0 +1,13 @@
+package com.paintourcolor.odle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paintourcolor/odle/OdleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paintourcolor.odle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

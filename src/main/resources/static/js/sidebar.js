6fb4e07b56962c37d640602c8623e5f2a9761756
JYMoon <damary016@gmail.com>
2023-02-10 17:50:43 +0900

    #8 feat: 메인 피드 html 및 css, js 생성

---
+++ src/main/resources/static/js/sidebar.js
@@ -0,0 +1,21 @@
+let menuToggle = document.querySelector('.toggle');
+let sidebar = document.querySelector('.sidebar')
+
+menuToggle.onclick = function() {
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+}
+
+
+//add active class in selected list item
+let list = document.querySelectorAll('.list');
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function() {
+        let j = 0;
+        while (j < list.length) {
+            list[j++].className = 'list';
+        }
+        list[i].className = 'list active';
+    }
+
+}

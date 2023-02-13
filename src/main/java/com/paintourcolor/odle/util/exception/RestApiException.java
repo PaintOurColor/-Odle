cf5f21e32dd0b637db4e637555fe75edc09b5d27
JYMoon <damary016@gmail.com>
2023-02-13 20:18:10 +0900

    #2 feat: IllegalArgumentException 예외처리 Handler 추가

--- src/main/java/com/paintourcolor/odle/util/exception/RestApiException.java
+++ src/main/java/com/paintourcolor/odle/util/exception/RestApiException.java
@@ -1,4 +1,15 @@
 package com.paintourcolor.odle.util.exception;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class RestApiException {
+    private String errorMessage;
+    private HttpStatus httpStatus;
+    private String errorCode;
 }

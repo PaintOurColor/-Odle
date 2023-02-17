750ce46b04a1393df2f59174fbed3d40f5bc805c
JYMoon <damary016@gmail.com>
2023-02-17 12:50:54 +0900

    #17 refactor: followUser 내 exception 변경

--- src/main/java/com/paintourcolor/odle/util/exception/RestApiExceptionHandler.java
+++ src/main/java/com/paintourcolor/odle/util/exception/RestApiExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.paintourcolor.odle.util.exception;
 
+import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,4 +19,23 @@ public class RestApiExceptionHandler {
         return new ResponseEntity(restApiException, HttpStatus.BAD_REQUEST);
 
     }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity UsernameNotFoundExceptionHandler(UsernameNotFoundException e) {
+        RestApiException restApiException = new RestApiException();
+        restApiException.setErrorCode("404");
+        restApiException.setHttpStatus(HttpStatus.NOT_FOUND);
+        restApiException.setErrorMessage(e.getMessage());
+        return new ResponseEntity(restApiException, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({DisabledException.class})
+    public ResponseEntity DisabledExceptionHandler(DisabledException e) {
+        RestApiException restApiException = new RestApiException();
+        restApiException.setErrorCode("403");
+        restApiException.setHttpStatus(HttpStatus.FORBIDDEN);
+        restApiException.setErrorMessage(e.getMessage());
+        return new ResponseEntity(restApiException, HttpStatus.FORBIDDEN);
+    }
+
 }

19880811ab22e24709b69928d8d8ef782c8a5c7d
sangwoo lee <lswoo0705@gmail.com>
2023-02-18 16:21:28 +0900

    #48 fix : getMusic 노래 정보 조회 기능 수정 완료

--- src/main/java/com/paintourcolor/odle/service/MusicService.java
+++ src/main/java/com/paintourcolor/odle/service/MusicService.java
@@ -1,22 +1,22 @@
 package com.paintourcolor.odle.service;
 
 import com.paintourcolor.odle.dto.mucis.response.MusicResponse;
-import com.paintourcolor.odle.entity.Music;
-import com.paintourcolor.odle.repository.MusicRepository;
+import com.paintourcolor.odle.entity.MelonKorea;
+import com.paintourcolor.odle.repository.MelonKoreaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MusicService implements MusicServiceInterface {
-    private final MusicRepository musicRepository;
+    private final MelonKoreaRepository melonRepository;
     // 노래 정보 조회
     @Override
-    public MusicResponse getMusic(Long musicId) {
-        Music music = musicRepository.findById(musicId).orElseThrow(
+    public MusicResponse getMusic(Long melonId) {
+        MelonKorea melon = melonRepository.findById(melonId).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 음악입니다.")
         );
 
-        return new MusicResponse(music.getTitle(), music.getSinger(), music.getCover());
+        return new MusicResponse(melon.getTitle(), melon.getSinger(), melon.getCover());
     }
 }

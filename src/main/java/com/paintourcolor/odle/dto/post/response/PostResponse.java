dd64476dd7c45352de8080d02c440212d321303a
JYMoon <damary016@gmail.com>
2023-02-22 09:20:45 +0900

    fix: PostResponse User및 Music에 대해 id가 아닌 data 반환하도록 변경 및 댓글수,생성시간,수정시간 변수 추가

--- src/main/java/com/paintourcolor/odle/dto/post/response/PostResponse.java
+++ src/main/java/com/paintourcolor/odle/dto/post/response/PostResponse.java
@@ -5,26 +5,45 @@ import com.paintourcolor.odle.entity.OpenOrEndEnum;
 import com.paintourcolor.odle.entity.Post;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostResponse {
 
     private final Long id;
-    private final Long user;
-    private final Long music;
+    private final Long userId;
+    private final String username;
+    private final String userProfileImage;
+    private final Long musicId;
+    private final String musicTitle;
+    private final String musicSinger;
+    private final String musicCover;
     private final Long likeCount;
     private final String content;
     private final OpenOrEndEnum openOrEnd;
     private final EmotionEnum emotion;
     private final String tagList;
+    private final Long commentCount;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
 
     public PostResponse(Post post, String tagList) {
         this.id = post.getId();
-        this.user = post.getUser().getId();
-        this.music = post.getMusic().getId();
+        this.userId = post.getUser().getId();
+        this.username = post.getUser().getUsername();
+        this.userProfileImage = post.getUser().getProfileImage();
+        this.musicId = post.getMusic().getId();
+        this.musicTitle = post.getMusic().getTitle();
+        this.musicSinger = post.getMusic().getSinger();
+        this.musicCover = post.getMusic().getCover();
         this.likeCount = post.getLikeCount();
         this.content = post.getContent();
         this.openOrEnd = post.getOpenOrEnd();
         this.emotion = post.getEmotion();
         this.tagList = tagList;
+        this.commentCount = post.getCommentCount();
+        this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
     }
 }

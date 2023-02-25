daf685d358e8043325946b17728a6c3d5378a845
sangwoo lee <lswoo0705@gmail.com>
2023-02-25 19:10:57 +0900

    #46 feat : inactivateMe 본인 계정 비활성화 기능 구현

--- src/main/java/com/paintourcolor/odle/service/UserServiceInterface.java
+++ src/main/java/com/paintourcolor/odle/service/UserServiceInterface.java
@@ -5,6 +5,7 @@ import com.paintourcolor.odle.dto.user.response.PostCountResponse;
 import com.paintourcolor.odle.dto.user.response.ProfilePostResponse;
 import com.paintourcolor.odle.dto.user.response.UserFollowOrUnfollowResponse;
 import com.paintourcolor.odle.dto.user.response.UserResponse;
+import com.paintourcolor.odle.entity.User;
 import com.paintourcolor.odle.security.UserDetailsImpl;
 import org.springframework.data.domain.Pageable;
 
@@ -18,7 +19,7 @@ public interface UserServiceInterface {
     void loginUser(UserLoginRequest userLoginRequest, HttpServletResponse response);
     void logoutUser(String token);  // ?
     void reissueToken(String refreshToken, HttpServletResponse response); //AccessToken 재발급
-    void inactivateUser(String username, UserInactivateRequest userInactivateRequest); // 이거 리퀘스트 같이 써도 되는지,,,
+    void inactivateMe(User user, UserInactivateRequest userInactivateRequest); // 이거 리퀘스트 같이 써도 되는지,,,
     List<UserResponse> getUsers(Pageable pageable);
     List<ProfilePostResponse> getProfilePosts(Long userId, Pageable pageable);
     PostCountResponse getPostCount(Long userId);

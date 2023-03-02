90d67671e1f5de06aea818a7f8bab513d59ac0e8
JYMoon <damary016@gmail.com>
2023-03-02 10:45:11 +0900

    #61 fix: refreshToken의 저장 및 관리를 DB -> Redis로 변경하며 Repository 삭제

--- src/main/java/com/paintourcolor/odle/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.paintourcolor.odle.repository;
-
-import com.paintourcolor.odle.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
-}

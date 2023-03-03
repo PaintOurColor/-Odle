e41d1074fdf70f1de663bd830e4fa4cbb4bb7b38
Yoon Yeojune <youj11@naver.com>
2023-03-03 22:05:28 +0900

    #169 feat : build.gradle - Jasypt 의존성 추가, jar 파일 생성 / JasyptConfig 생성 / 테스트코드 주석처리

--- src/test/java/com/paintourcolor/odle/controller/UserControllerTest.java
+++ src/test/java/com/paintourcolor/odle/controller/UserControllerTest.java
@@ -1,20 +1,20 @@
-package com.paintourcolor.odle.controller;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserControllerTest {
-
-    @Test
-    void signUp() {
-    }
-
-    @Test
-    void loginUser() {
-    }
-
-    @Test
-    void logoutUser() {
-    }
-}
+//package com.paintourcolor.odle.controller;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class UserControllerTest {
+//
+//    @Test
+//    void signUp() {
+//    }
+//
+//    @Test
+//    void loginUser() {
+//    }
+//
+//    @Test
+//    void logoutUser() {
+//    }
+//}

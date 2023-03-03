32285703d42029934bd3eee52fa6aad934b14b9e
diddnwjd <yang965273@gmail.com>
2023-03-03 17:55:21 +0900

    #166 feat : 태그 검색 후 게시글 조회 프론트와 연결

---
+++ src/main/resources/static/js/post_search.js
@@ -0,0 +1,103 @@
+const a = document.location.href.split('=')[1];
+const word = decodeURI(decodeURIComponent(a)); // 한글로 나오게 해줌
+
+let login_search_userId;
+
+// 페이지가 로딩될 때 실행되는 거
+jQuery(document).ready(function ($) {
+    getMySimpleProfile1();
+    getTagInfo()
+    getPostSearchCount();
+    getPostSearchList();
+});
+
+// 상단바 프로필 보기
+function getMySimpleProfile1() {
+    var settings = {
+        "url": "http://localhost:8080/users/profile/simple",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": localStorage.getItem('accessToken')
+        },
+    };
+
+    $.ajax(settings).done(function (response, status) {
+        login_search_userId = response['userId']
+        $('#myProfileUsername4').text(response['username'])
+        $('#myProfileImage4').attr('src', response['profileImage'] == null ? 'http://bwptedu.com/assets/image/default-profile.jpg' : response['profileImage'])
+        if (status === 403) { // 권한이 없는 것이니까 로그인으로 보내면 됨
+            window.location = "/login.html"
+        }
+        const userId = response['userId']
+        $('#myProfile9').attr('onclick', `window.location.href='./profile.html?userId=${userId}'`)
+        $('#myProfile10').attr('onclick', `window.location.href='./profile.html?userId=${userId}'`)
+        $('#myProfile11').attr('onclick', `window.location.href='./profile.html?userId=${userId}'`)
+    });
+}
+
+// 태그 이름
+function getTagInfo() {
+    $('.post_search__tagName').append("#" + word)
+}
+
+// 검색된 게시글 개수
+function getPostSearchCount() {
+    var settings = {
+        "url": `http://localhost:8080/posts/search/${word}/post-count`,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": localStorage.getItem('accessToken')
+        },
+    };
+
+    $.ajax(settings).done(function (response) {
+        const postCount = response['profilePostCount']
+        $('#post_search_count').append(postCount)
+    }).error(function () {
+        $('#post_search_count').append("0")
+    });
+}
+
+// 검색창 클릭
+function getPostSearchList() {
+    const settings = {
+        "url": `http://localhost:8080/posts/search/${word}`,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": localStorage.getItem('accessToken')
+        },
+    };
+
+    $.ajax(settings).done(function (response) {
+        $('#post_search-posts').empty()
+        for (let i = 0; i < response.length; i++) {
+            const obj = response[i]
+            const postId = obj['postId']
+            const title = obj['title']
+            const singer = obj['singer']
+            const cover = obj['cover']
+
+            const tempHtml = `
+                            <div id="post_search_popup_btn_${postId}" class="post_search__photo" onclick="open_post_search_popup(${postId})">
+                            <a href="#개별게시글링크">
+                                <img src="${cover}" />
+                            </a>
+                            <span>${title}
+                            <br>-${singer}</span>
+                        </div>
+            `
+            $('#post_search-posts').append(tempHtml)
+        }
+    }).error(function () {
+        alert("검색된 게시물이 없습니다.")
+    });
+}
+
+// 검색창에 입력한 단어 주소창으로 보내기
+function getSearchWord() {
+    const word = $(`#search_word`).val();
+    return window.location.href=`./post_search.html?query=${decodeURI(decodeURIComponent(word))}`
+}

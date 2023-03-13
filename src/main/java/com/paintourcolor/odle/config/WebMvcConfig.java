f3246803bb1e6ca0d3fc3f55008e8ea2561648d0
sangwoo lee <lswoo0705@gmail.com>
2023-03-13 11:49:32 +0900

    feat : XSS Filter 구현

--- src/main/java/com/paintourcolor/odle/config/WebMvcConfig.java
+++ src/main/java/com/paintourcolor/odle/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.paintourcolor.odle.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +14,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public MultipartResolver multipartResolver() {
         return new CommonsMultipartResolver();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        // MappingJackson2HttpMessageConverter Default ObjectMapper 설정 및 ObjectMapper Config 설정
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+        objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(objectMapper);
     }
 }
